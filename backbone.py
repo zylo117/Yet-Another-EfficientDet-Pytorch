616adc8666f60011f84891d320c921fea975f935
Zylo Cheung <zylo117@hotmail.com>
2020-04-12 22:48:47 +0800

    old code cleanup

--- backbone.py
+++ backbone.py
@@ -52,14 +52,6 @@ class EfficientDetBackbone(nn.Module):
 
         self.anchors = Anchors(anchor_scale=self.anchor_scale[compound_coef], **kwargs)
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
         self.backbone_net = EfficientNet(self.backbone_compound_coef[compound_coef], load_weights)
 
     def freeze_bn(self):
