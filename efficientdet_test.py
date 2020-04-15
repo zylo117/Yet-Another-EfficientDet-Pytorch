85ac208de6253b1b578817550e0db03e4708d787
zylo117 <5008abz723>
2020-04-15 15:31:49 +0800

    set force_input_size to None by default; supports custom anchor_ratios and anchor_scales in efficientdet_test.py

--- efficientdet_test.py
+++ efficientdet_test.py
@@ -16,8 +16,12 @@ from efficientdet.utils import BBoxTransform, ClipBoxes
 from utils.utils import preprocess, invert_affine, postprocess
 
 compound_coef = 0
-force_input_size = 1920  # set None to use default size
+force_input_size = None  # set None to use default size
 img_path = 'test/img.png'
+
+# replace this part with your project's anchor config
+anchor_ratios = [(1.0, 1.0), (1.4, 0.7), (0.7, 1.4)]
+anchor_scales = [2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)]
 
 threshold = 0.2
 iou_threshold = 0.2
@@ -50,7 +54,8 @@ else:
 
 x = x.to(torch.float32 if not use_float16 else torch.float16).permute(0, 3, 1, 2)
 
-model = EfficientDetBackbone(compound_coef=compound_coef, num_classes=len(obj_list))
+model = EfficientDetBackbone(compound_coef=compound_coef, num_classes=len(obj_list),
+                             ratios=anchor_ratios, scales=anchor_scales)
 model.load_state_dict(torch.load(f'weights/efficientdet-d{compound_coef}.pth'))
 model.requires_grad_(False)
 model.eval()
