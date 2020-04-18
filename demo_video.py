a5d1f4e123d1b5fdb8d64c7db8ba610e1f66ba15
Kwang Myung, Jeon <lordet01@gmail.com>
2020-04-18 13:00:32 +0900

    added colored obj feature

--- demo_video.py
+++ demo_video.py
@@ -4,7 +4,7 @@
 Simple Inference Script of EfficientDet-Pytorch
 """
 import time
-
+import os 
 import torch
 from torch.backends import cudnn
 
@@ -15,9 +15,9 @@ import numpy as np
 from efficientdet.utils import BBoxTransform, ClipBoxes
 from util.utils import preprocess_single_image, invert_affine, postprocess
 
-compound_coef = 0
+compound_coef = 2
 force_input_size = None  # set None to use default size
-img_path = 0#"./test/cross.mp4" # or camera id
+img_path = "./test/cross.mp4" # or camera id
 
 # replace this part with your project's anchor config
 anchor_ratios = [(1.0, 1.0), (1.4, 0.7), (0.7, 1.4)]
@@ -41,6 +41,8 @@ obj_list = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train'
             'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink',
             'refrigerator', '', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
             'toothbrush']
+np.random.seed(3)
+colors = np.random.uniform(0, 255, size=(len(obj_list), 3))
 
 # tf bilinear interpolation is different from any other's, just make do
 input_sizes = [512, 640, 768, 896, 1024, 1280, 1280, 1536]
@@ -113,12 +115,12 @@ while vid.grab():
     # draw bbox on frame and display
     for j in range(len(pred['rois'])):
         (x1, y1, x2, y2) = pred['rois'][j].astype(np.int)
-        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 0), 2)
+        cv2.rectangle(img, (x1, y1), (x2, y2), colors[pred['class_ids'][j]], 2)
         obj = obj_list[pred['class_ids'][j]]
         score = float(pred['scores'][j])
         cv2.putText(img, '{}, {:.3f}'.format(obj, score),
                     (x1, y1 + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    (255, 255, 0), 1)
+                    colors[pred['class_ids'][j]], 1)
 
     cv2.imshow('Video', img)
     key = cv2.waitKey(1) & 0xFF
