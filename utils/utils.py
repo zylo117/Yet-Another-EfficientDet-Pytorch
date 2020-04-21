cab17ef4b2b3a9eb81dee59a1a13b41d0800b283
mashiwei <mashiwei@jd.com>
2020-04-21 18:52:24 +0800

    add variance_scaling initializer in Regressor/Classifier, add constant initializer in cls bias, same with tf version. And variance_scaling initializer will make training more stable(early stage).

--- utils/utils.py
+++ utils/utils.py
@@ -13,6 +13,21 @@ import uuid
 
 from utils.sync_batchnorm import SynchronizedBatchNorm2d
 
+from torch.nn.init import _calculate_fan_in_and_fan_out, _no_grad_normal_
+import math
+
+
+def variance_scaling_(tensor, gain=1.):
+    # type: (Tensor, float) -> Tensor
+    r"""
+    initializer for SeparableConv in Regressor/Classifier
+    reference: https://keras.io/zh/initializers/  VarianceScaling
+    """
+    fan_in, fan_out = _calculate_fan_in_and_fan_out(tensor)
+    std = math.sqrt(gain / float(fan_in))
+
+    return _no_grad_normal_(tensor, 0., std)
+
 
 def invert_affine(metas: Union[float, list, tuple], preds):
     for i in range(len(preds)):
@@ -209,7 +224,14 @@ def init_weights(model):
         is_conv_layer = isinstance(module, nn.Conv2d)
 
         if is_conv_layer:
-            nn.init.kaiming_uniform_(module.weight.data)
+            if "conv_list" or "header" in name:
+                nn.init.variance_scaling_(module.weight.data)
+            else:
+                nn.init.kaiming_uniform_(module.weight.data)
 
             if module.bias is not None:
-                module.bias.data.zero_()
+                if "classifier.header" in name:
+                    bias_value = -np.log((1 - 0.01) / 0.01)
+                    torch.nn.init.constant_(module.bias, bias_value)
+                else:
+                    module.bias.data.zero_()
