beb5b04450d44f43dd4b727af2b36a8a02ea56be
Takashi Kaneda <takashi.kaneda@gmail.com>
2020-04-22 10:05:35 +0000

    Add weights download script

---
+++ get_weights.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir weights
+
+wget https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d0.pth -P ./weights
+wget https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d1.pth -P ./weights
+wget https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d2.pth -P ./weights
+wget https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d3.pth -P ./weights
+wget https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d4.pth -P ./weights
+wget https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d5.pth -P ./weights
+wget https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d6.pth -P ./weights
+wget https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d7.pth -P ./weights
