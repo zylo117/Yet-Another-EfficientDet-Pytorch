b349ef0de86ec52742ecc46cbd9059ec3f5edb1b
zylo117 <zylo117@hotmail.com>
2020-08-17 14:29:53 +0800

    fix opencv issue

--- utils/utils.py
+++ utils/utils.py
@@ -133,6 +133,8 @@ def display(preds, imgs, obj_list, imshow=True, imwrite=False):
         if len(preds[i]['rois']) == 0:
             continue
 
+        imgs[i] = imgs[i].copy()
+
         for j in range(len(preds[i]['rois'])):
             (x1, y1, x2, y2) = preds[i]['rois'][j].astype(np.int)
             obj = obj_list[preds[i]['class_ids'][j]]
