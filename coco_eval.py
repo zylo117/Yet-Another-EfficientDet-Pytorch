49805438d7460b3a94c0f65154ae797f7d66f667
zylo117 <zylo117@hotmail.com>
2020-10-22 11:33:15 +0800

    fix coco_eval for not using specified mean and std

--- coco_eval.py
+++ coco_eval.py
@@ -62,7 +62,7 @@ def evaluate_coco(img_path, set_name, image_ids, coco, model, threshold=0.05):
         image_info = coco.loadImgs(image_id)[0]
         image_path = img_path + image_info['file_name']
 
-        ori_imgs, framed_imgs, framed_metas = preprocess(image_path, max_size=input_sizes[compound_coef])
+        ori_imgs, framed_imgs, framed_metas = preprocess(image_path, max_size=input_sizes[compound_coef], mean=params['mean'], std=params['std'])
         x = torch.from_numpy(framed_imgs[0])
 
         if use_cuda:
