dc5a560cd1c69b7e5330f67779ed2cbbc8b8cf50
Fabiana Lanotte <fabiana.lanotte@prometeia.com>
2020-11-20 17:37:15 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+with open('readme.md') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt', 'r') as fh:
+    requirements = fh.read().split('\n')
+setup(
+    name='yet_another_efficientdet',
+    version='0.1.0',
+    py_modules=['backbone'],
+    packages=find_packages(include=['efficientdet', 'efficientdet.*', 'efficientnet', 'efficientnet.*', 'backbone', 'utils', 'utils.*']),
+    url='https://github.com/zylo117/Yet-Another-EfficientDet-Pytorch',
+    license='',
+    author='zylo117',
+    author_email='',
+    description='Yet-Another-EfficientDet-Pytorch',
+    long_description=readme,
+    install_requires=requirements,
+
+)
