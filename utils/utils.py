47f6bf3ccc118fbc2057112c507f419ff3e6e275
zylo117 <zylo117@hotmail.com>
2020-12-01 11:39:57 +0800

    fix bgr order in display function

--- utils/utils.py
+++ utils/utils.py
@@ -66,8 +66,8 @@ def aspectaware_resize_padding(image, width, height, interpolation=None, means=N
 
 
 def preprocess(*image_path, max_size=512, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
-    ori_imgs = [cv2.imread(img_path)[..., ::-1] for img_path in image_path]
-    normalized_imgs = [(img / 255 - mean) / std for img in ori_imgs]
+    ori_imgs = [cv2.imread(img_path) for img_path in image_path]
+    normalized_imgs = [(img[..., ::-1] / 255 - mean) / std for img in ori_imgs]
     imgs_meta = [aspectaware_resize_padding(img, max_size, max_size,
                                             means=None) for img in normalized_imgs]
     framed_imgs = [img_meta[0] for img_meta in imgs_meta]
